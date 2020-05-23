8eb93937ddcdb60a9b383800da4e4cc19c450375
Ligy <791444095@qq.com>
2020-05-23 13:10:27 +0800

    first time

---
+++ Test/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ligy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Init
+            ToolTipWithPictureOrGif pictureOrGif = new ToolTipWithPictureOrGif();
+
+            //picture or gif
+            pictureOrGif.Binding(label1,Image.FromFile(Path.Combine(Application.StartupPath,"TestPic.jpg")));
+        }
+    }
+}
